f8fa9e1f437976974b8001ef545433b626cb30a5
Umang Parmar <umangjparmar@gmail.com>
2020-01-03 10:26:29 +0530

    add game to storage for lookup when new game is created

--- game/api.go
+++ game/api.go
@@ -21,6 +21,7 @@ func CreateNewGame(c *gin.Context) {
 	if gameInstance.PlayersCount == 0 {
 		gameInstance.PlayersCount = 2
 	}
+	addGameInstance(gameInstance)
 	c.JSON(http.StatusCreated, gin.H{"Game Instance": gameInstance})
 }
 
