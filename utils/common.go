6c6cc276be90ebcb8b63ff52794e66e2b132f3de
Umang Parmar <umangjparmar@gmail.com>
2020-01-05 23:26:25 +0530

    Add util funcs to check of given position is on edge or corner

--- utils/common.go
+++ utils/common.go
@@ -9,3 +9,25 @@ func Contains(arr []string, val string) bool {
 	}
 	return false
 }
+
+// IsCorner checks if given position is corner or not
+func IsCorner(m int, n int, x int, y int) bool {
+	if (x == 0 && y == 0) ||
+		(x == m-1 && y == 0) ||
+		(x == 0 && y == n-1) ||
+		(x == m-1 && y == n-1) {
+		return true
+	}
+	return false
+}
+
+// IsOnEdge checks if given position is on edge or not
+func IsOnEdge(m int, n int, x int, y int) bool {
+	if (x == 0 && y != 0 && y != n-1) ||
+		(x != 0 && x != m-1 && y == 0) ||
+		(x != 0 && x != m-1 && y == n-1) ||
+		(x == m-1 && y != 0 && y != n-1) {
+		return true
+	}
+	return false
+}
