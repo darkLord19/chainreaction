9b352cb50f0ccf698374ab6b37fa14d935380c6d
Umang Parmar <umangjparmar@gmail.com>
2020-01-05 22:52:29 +0530

    Add constant package to store constants

---
+++ constant/constant.go
@@ -0,0 +1,16 @@
+package constant
+
+const (
+	red    = "#f44336"
+	pink   = "#e91e63"
+	purple = "#9c27b0"
+	indigo = "#3f51b5"
+	blue   = "#2196f3"
+	cyan   = "#00bcd4"
+	teal   = "#009688"
+	green  = "#4caf50"
+	yellow = "#ffeb3b"
+	orange = "#ff9800"
+	brown  = "#795548"
+	grey   = "#9e9e9e"
+)
