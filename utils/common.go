b02fab798867398f20e3811bf9f9adbd11b8ffe5
Umang Parmar <umangjparmar@gmail.com>
2020-01-07 15:57:45 +0530

    While dequeuing directly return ints

--- utils/common.go
+++ utils/common.go
@@ -1,5 +1,46 @@
 package utils
 
+import "sync"
+
+// Pair struct to
+type Pair struct {
+	X int
+	Y int
+}
+
+// Queue the queue of int pairs
+type Queue struct {
+	items []Pair
+	lock  sync.RWMutex
+}
+
+// NewQueue creates a new Queue
+func (s *Queue) NewQueue() *Queue {
+	s.items = []Pair{}
+	return s
+}
+
+// Enqueue adds an Item to the end of the queue
+func (s *Queue) Enqueue(t Pair) {
+	s.lock.Lock()
+	s.items = append(s.items, t)
+	s.lock.Unlock()
+}
+
+// Dequeue removes a Pair from the start of the queue
+func (s *Queue) Dequeue() (int, int) {
+	s.lock.Lock()
+	item := s.items[0]
+	s.items = s.items[1:len(s.items)]
+	s.lock.Unlock()
+	return item.X, item.Y
+}
+
+// IsEmpty returns true if the queue is empty
+func (s *Queue) IsEmpty() bool {
+	return len(s.items) == 0
+}
+
 // Contains checks if a slice containse given value
 func Contains(arr []string, val string) bool {
 	for _, item := range arr {
