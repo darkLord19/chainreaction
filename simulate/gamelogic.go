03e25eff05b561b2e059728284023da563228f1b
Umang Parmar <umangjparmar@gmail.com>
2020-01-07 15:58:33 +0530

    update game board state after each event and do chaining

--- simulate/gamelogic.go
+++ simulate/gamelogic.go
@@ -8,7 +8,64 @@ import (
 )
 
 func updateBoard(board *[][]game.Pixel, x int, y int) {
+	q := utils.NewQueue()
+	q.Enqueue(utils.Pair{x, y})
+
+	color := (*board)[x][y].Color
+
+	m := len(*board)
+	n := len((*board)[0])
+
+	for !q.IsEmpty() {
+		x, y = q.Dequeue()
+		(*board)[x][y].DotCount++
+
+		cnt := (*board)[x][y].DotCount
+
+		switch cnt {
+		case 2:
+			if utils.IsCorner(m, n, x, y) {
+				updatePixelState(board, x, y, color)
+				q.Enqueue(utils.Pair{x, y})
+			}
+		case 3:
+			if utils.IsOnEdge(m, n, x, y) {
+				updatePixelState(board, x, y, color)
+				q.Enqueue(utils.Pair{x, y})
+			}
+		case 4:
+			updatePixelState(board, x, y, color)
+			q.Enqueue(utils.Pair{x, y})
+		}
+
+	}
+
 	return
+}
+
+func updatePixelState(board *[][]game.Pixel, x int, y int, color string) {
+	(*board)[x][y].DotCount = 0
+	(*board)[x][y].Color = ""
+
+	m := len(*board)
+	n := len((*board)[0])
+
+	if x > 0 {
+		(*board)[x-1][y].DotCount++
+		(*board)[x-1][y].Color = color
+	}
+	if y > 0 {
+		(*board)[x][y-1].DotCount++
+		(*board)[x][y-1].Color = color
+	}
+	if x < m-1 {
+		(*board)[x+1][y].DotCount++
+		(*board)[x+1][y].Color = color
+	}
+	if y < n-1 {
+		(*board)[x][y+1].DotCount++
+		(*board)[x][y+1].Color = color
+	}
 }
 
 // ChainReaction is called after each move and spreads the orbs on the board
@@ -19,22 +76,7 @@ func ChainReaction(gameInstance *game.Instance, x int, y int) error {
 		return fmt.Errorf("Given positions x %v and y %v are out of range", x, y)
 	}
 
-	board[x][y].DotCount++
-
-	cnt := board[x][y].DotCount
-
-	switch cnt {
-	case 2:
-		if utils.IsCorner(len(board), len(board[0]), x, y) {
-			updateBoard(&board, x, y)
-		}
-	case 3:
-		if utils.IsOnEdge(len(board), len(board[0]), x, y) {
-			updateBoard(&board, x, y)
-		}
-	case 4:
-		updateBoard(&board, x, y)
-	}
+	updateBoard(&board, x, y)
 
 	return nil
 }
