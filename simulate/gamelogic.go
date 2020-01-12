ba084ada3d41fb318916197e784fb5bd01a4cbe5
Umang Parmar <umangjparmar@gmail.com>
2020-01-12 17:06:51 +0530

    Fix gamelogic

--- simulate/gamelogic.go
+++ simulate/gamelogic.go
@@ -14,26 +14,25 @@ func updateBoard(board *[][]game.Pixel, x int, y int, color string) {
 	m := len(*board)
 	n := len((*board)[0])
 
+	(*board)[x][y].DotCount++
+	(*board)[x][y].Color = color
+
 	for !q.IsEmpty() {
 		x, y = q.Dequeue()
-		(*board)[x][y].DotCount++
 
 		cnt := (*board)[x][y].DotCount
 
 		switch cnt {
 		case 2:
 			if utils.IsCorner(m, n, x, y) {
-				updatePixelState(board, x, y, color)
-				q.Enqueue(utils.Pair{x, y})
+				updatePixelState(board, x, y, color, q)
 			}
 		case 3:
 			if utils.IsOnEdge(m, n, x, y) {
-				updatePixelState(board, x, y, color)
-				q.Enqueue(utils.Pair{x, y})
+				updatePixelState(board, x, y, color, q)
 			}
 		case 4:
-			updatePixelState(board, x, y, color)
-			q.Enqueue(utils.Pair{x, y})
+			updatePixelState(board, x, y, color, q)
 		}
 
 	}
@@ -41,7 +40,7 @@ func updateBoard(board *[][]game.Pixel, x int, y int, color string) {
 	return
 }
 
-func updatePixelState(board *[][]game.Pixel, x int, y int, color string) {
+func updatePixelState(board *[][]game.Pixel, x int, y int, color string, q *utils.Queue) {
 	(*board)[x][y].DotCount = 0
 	(*board)[x][y].Color = ""
 
@@ -51,18 +50,22 @@ func updatePixelState(board *[][]game.Pixel, x int, y int, color string) {
 	if x > 0 {
 		(*board)[x-1][y].DotCount++
 		(*board)[x-1][y].Color = color
+		q.Enqueue(utils.Pair{x - 1, y})
 	}
 	if y > 0 {
 		(*board)[x][y-1].DotCount++
 		(*board)[x][y-1].Color = color
+		q.Enqueue(utils.Pair{x, y - 1})
 	}
 	if x < m-1 {
 		(*board)[x+1][y].DotCount++
 		(*board)[x+1][y].Color = color
+		q.Enqueue(utils.Pair{x + 1, y})
 	}
 	if y < n-1 {
 		(*board)[x][y+1].DotCount++
 		(*board)[x][y+1].Color = color
+		q.Enqueue(utils.Pair{x, y + 1})
 	}
 }
 
