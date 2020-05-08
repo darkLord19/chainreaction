1f22e1200a16f05eabbae1dac4d78beb49eb67df
Umang Parmar <umangjparmar@gmail.com>
2020-05-08 22:07:23 +0530

    reflect restructure changes in data store

--- datastore/ds.go
+++ datastore/ds.go
@@ -4,27 +4,27 @@ import (
 	"strings"
 
 	"github.com/Pallinder/go-randomdata"
-	"github.com/chainreaction/game"
+	"github.com/chainreaction/models"
 )
 
 var (
-	allGameInstances    map[string]*game.Instance
-	activeGameInstances map[string]*game.Instance
+	allGameInstances    map[string]*models.Instance
+	activeGameInstances map[string]*models.Instance
 )
 
 func init() {
-	allGameInstances = make(map[string]*game.Instance)
-	activeGameInstances = make(map[string]*game.Instance)
+	allGameInstances = make(map[string]*models.Instance)
+	activeGameInstances = make(map[string]*models.Instance)
 }
 
 // GetGameInstance returns game instance from instance id
-func GetGameInstance(iid string) (*game.Instance, bool) {
+func GetGameInstance(iid string) (*models.Instance, bool) {
 	val, ok := activeGameInstances[iid]
 	return val, ok
 }
 
 // AddGameInstance adds game instance in a data store indexed by instance id
-func AddGameInstance(gameInstance *game.Instance) {
+func AddGameInstance(gameInstance *models.Instance) {
 	allGameInstances[gameInstance.RoomName] = gameInstance
 	activeGameInstances[gameInstance.RoomName] = gameInstance
 }
