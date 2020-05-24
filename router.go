0fee422bd754b401bbfbbf685f25be4daf6d55ce
Umang Parmar <umangjparmar@gmail.com>
2020-05-24 17:30:47 +0530

    gracefully shutdown server

--- router.go
+++ router.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/chainreaction/api"
 	"github.com/chainreaction/datastore"
 	"github.com/gin-gonic/gin"
 )
 
-func setupRouter() {
+func setupRouter() *gin.Engine {
 	router := gin.Default()
 	router.GET("/new", api.CreateNewGame)
 	// here it is /games/:name due to limitation of httprouter used by gin because
@@ -16,10 +24,45 @@ func setupRouter() {
 	router.GET("/games/:name/play", api.StartGamePlay)
 	router.GET("/games/:name/colors", api.GetAvailableColors)
 
-	router.Run()
+	return router
 }
 
 func main() {
-	setupRouter()
+	router := setupRouter()
 	go datastore.Cleanup()
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
+
 }
